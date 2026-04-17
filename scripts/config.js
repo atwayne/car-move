8568f8b4203f4116775352dcf0df34122cbb397d
Wayne Wang <nkgailei@gmail.com>
2026-04-17 21:26:02 +0800

    color palette update

--- scripts/config.js
+++ scripts/config.js
@@ -4,8 +4,6 @@ export const CELL_SIZE = 64;
 export const CELL_GAP = 6;
 export const STEP_ANIMATION_MS = 240;
 
-export const COLOR_PALETTE = ["red", "green", "yellow", "purple", "black", "blue"];
-
 export const POLICE_CAR_ID = "police-car";
 
 export const POLICE_CAR = {
@@ -14,7 +12,7 @@ export const POLICE_CAR = {
   orientation: "vertical",
   row: 0,
   col: 3,
-  color: "#8aa2c8",
+  color: "#134686",
   isPolice: true,
 };
 
@@ -24,10 +22,24 @@ export const DEV_MOVE_LIST = [
 ];
 
 export const COLOR_MAP = {
-  red: "#cf4a4a",
-  green: "#4caf6f",
-  yellow: "#ceb454",
-  purple: "#9a62c8",
-  black: "#212832",
-  blue: "#4f80dd",
+  maroon: "#810000",
+  red: "#CE2626",
+  peach: "#E97171",
+  orange: "#FF4400",
+  brown: "#8B4513",
+  beige:"#F5F5DC",
+  yellow: "#F8DE22",
+  sage: "#B2AC88",
+  green: "#6BCB77",
+  teal: "#03AED2",
+  mint: "#A2D5C6",
+  blue: "#0079FF",
+  navy: "#0D1164",
+  purple: "#7E1891",
+  pink: "#D12052",
+  black: "#040303",
+  grey: "#808080",
+  white: "#F0F0F0"
 };
+
+export const COLOR_PALETTE = Object.keys(COLOR_MAP);
